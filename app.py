505ba14e2187df08a012458aab82d44a6af6b711
arkay-works <arkay176@gmail.com>
2025-02-06 11:31:08 +0530

    Server

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import requests
+import random
+
+app = Flask(__name__)
+
+# List of IP addresses
+ip_addresses = [
+    'http://192.168.0.3/rotate',
+    'http://192.168.0.4/rotate',
+    'http://192.168.0.5/rotate',
+    'http://192.168.0.6/rotate',
+    #'http://192.168.86.212/rotate'
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/rotate', methods=['POST'])
+def rotate():
+#     # Randomly select an IP address
+    selected_ip = random.choice(ip_addresses)
+    
+    # Make a request to the selected IP address
+    resp = requests.get(selected_ip)
+    
+    # Return the response status code and text
+    return f"Requested IP: {selected_ip}<br>Response: {resp.status_code} - {resp.text}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- main.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-resp = requests.get('http://192.168.86.208/rotate')
-
-print(resp)
